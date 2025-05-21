4dc95925a072142d1818e277cbeea937fe46eff6
Aaryan Ved Bhalla <aaryan14.bhalla@gmail.com>
2025-05-21 10:52:03 +0530

    remvove profane file

---
+++ checkWhitespace.java
@@ -0,0 +1,9 @@
+public class checkWhitespace
+{
+    public static void main(String args[])
+    {
+        char c=' ';
+        boolean b=Character.isWhitespace(c);
+        System.out.println(b);
+    }
+}
